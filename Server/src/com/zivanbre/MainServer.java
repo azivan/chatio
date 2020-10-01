cb84dfadf5efa6432d96a04f7ca6677c4f228712
azivan <info.azivanovic@gmail.com>
2020-10-01 23:40:07 +0200

    Second commit

--- Server/src/com/zivanbre/MainServer.java
+++ Server/src/com/zivanbre/MainServer.java
@@ -12,34 +12,24 @@ public class MainServer {
             System.out.println("Waiting for connection on: " + hostName + ":" + port);
             ServerSocket serverSocket = new ServerSocket(port);
             Socket clientSocket = serverSocket.accept();
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            BackServer server = new BackServer(clientSocket);
-            Thread thread = new Thread() {
-                @Override
-                public void run() {
-                    try {
-                        clientHandler(out, in);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            };
-            thread.start();
+            System.out.println("New terminal connected");
+            PrintWriter output = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            clientHandler(output, input);
         } catch (IOException e) {
             System.out.println("Error when trying to listen to port: " + port);
             e.printStackTrace();
         }
     }
 
-    private static void clientHandler(PrintWriter out, BufferedReader in) throws IOException {
-        String inputLine;
-        while ((inputLine = in.readLine()) != null) {
-            out.println("Server: " + inputLine);
-            System.out.println("Client typed: " + inputLine);
-            if ( inputLine.toLowerCase().equals("quit") ) {
+    public static void clientHandler(PrintWriter output, BufferedReader input) throws IOException {
+        String clientInput;
+        while ((clientInput = input.readLine()) != null) {
+            System.out.println("Client typed: " + clientInput);
+            output.println("Server: " + clientInput);
+            if ( clientInput.equals("quit") ) {
                 System.out.println("Client has quit the server");
-                break;
+
             }
         }
     }
