8a093ce0714cdd5016128077c8515c18b598e6a2
azivan <info.azivanovic@gmail.com>
2020-10-01 23:55:25 +0200

    Client input and Server output named.

--- Server/src/com/zivanbre/MainServer.java
+++ Server/src/com/zivanbre/MainServer.java
@@ -12,7 +12,7 @@ public class MainServer {
             System.out.println("Waiting for connection on: " + hostName + ":" + port);
             ServerSocket serverSocket = new ServerSocket(port);
             Socket clientSocket = serverSocket.accept();
-            System.out.println("New terminal connected");
+            System.out.println("New terminal connected on port: " + port);
             PrintWriter output = new PrintWriter(clientSocket.getOutputStream(), true);
             BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             clientHandler(output, input);
@@ -23,13 +23,13 @@ public class MainServer {
     }
 
     public static void clientHandler(PrintWriter output, BufferedReader input) throws IOException {
-        String clientInput;
+        String clientInput, clientOutput;
         while ((clientInput = input.readLine()) != null) {
             System.out.println("Client typed: " + clientInput);
-            output.println("Server: " + clientInput);
+            output.println("You typed: " + clientInput);
+            output.println("Server responded: " + clientInput);
             if ( clientInput.equals("quit") ) {
                 System.out.println("Client has quit the server");
-
             }
         }
     }
